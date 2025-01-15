3ff1702662d32032500d9c4602733a945cdc8558
rnvllz <vaaron23@gmail.com>
2025-01-15 23:39:24 +0800

    lil fix

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.SwingUtilities;
 public class Main {
     public static void main(String[] args) {
 
-        // Create and display the home screen on the event dispatch thread.
+        // Create and display the home screen.
         SwingUtilities.invokeLater(() -> {
             new HomeScreen().setVisible(true);
         });
